aa3d021fb836a495cc5dbef2bebd1bc248b39610
christopherstlot2003 <christopherstlot2003@gmail.com>
2026-06-25 20:58:07 -0400

    fix allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://civis-zr8z.onrender.com'] 
+ALLOWED_HOSTS = ['civis-zr8z.onrender.com'] 
 
 
 # Application definition
