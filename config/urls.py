f100af4734273f5e8cf6d4e5cbf26d835f969e2c
christopherstlot2003 <christopherstlot2003@gmail.com>
2026-06-25 21:41:02 -0400

    changement url

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from civis import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('gestion-christopher/', admin.site.urls),
     path('',views.home,name='accueil'),
     path('detail_chef/<int:id>/',views.detail_chef,name='detail_chef'),
     path('civilisation',views.all_civilisation,name='civilisation'),
